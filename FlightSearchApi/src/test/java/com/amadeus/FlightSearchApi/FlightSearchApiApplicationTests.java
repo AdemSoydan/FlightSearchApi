578b55f2e1ccfa2dd554baf1a62a9543579c6f77
ademsoydan <86920390+AdemSoydan@users.noreply.github.com>
2023-12-21 23:56:59 +0300

    Initialize commit

---
+++ FlightSearchApi/src/test/java/com/amadeus/FlightSearchApi/FlightSearchApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amadeus.FlightSearchApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightSearchApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
